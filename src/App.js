3a487cf308b3abae8afaf0a489d1241916919930
Jusun Kang <wntjs0305@gmail.com>
2019-06-10 14:46:28 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -55,31 +55,58 @@ const styles = theme => ({
     overflowX: "auto"
   },
   table: {
-    minwidth: 1080
+    minWidth: 1080
   }
 })
 
-function App() {
-  // const { classes } = this.props;
-  return (
-    <Paper className = { this.props.root }>
-      <Table className = { this.table }>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-            { customers.map(c => { return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); }) }
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className = {classes.root}>
+        <Table className = {classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+              { customers.map(c => { return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); }) }
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
+
+// function App() {
+//   // const { classes } = this.props;
+//   return (
+//     // <Paper className = { this.props.root }>
+//     //   <Table className = { this.table }>
+//     <Paper>
+//       <Table>
+//         <TableHead>
+//           <TableRow>
+//             <TableCell>번호</TableCell>
+//             <TableCell>이미지</TableCell>
+//             <TableCell>이름</TableCell>
+//             <TableCell>생년월일</TableCell>
+//             <TableCell>성별</TableCell>
+//             <TableCell>직업</TableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>
+//             { customers.map(c => { return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); }) }
+//         </TableBody>
+//       </Table>
+//     </Paper>
+//   );
+// }
 
 export default withStyles(styles)(App);
