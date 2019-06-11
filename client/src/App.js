ddddb837f7c89a32070b01a51d49620987b7d1b6
Jusun Kang <wntjs0305@gmail.com>
2019-06-11 18:37:24 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-// import logo from './logo.svg';
 import Customer from './components/Customer'
 import './App.css';
 import Paper from '@material-ui/core/Paper';
@@ -9,49 +8,11 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
-import { getThemeProps } from '@material-ui/styles';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <Customer/>
-//       </div>
-//     );
-//   }
-// }
-
-const customers = [
-  {
-  'id': '1',
-  'name': '홍길동',
-  'image': 'https://placeimg.com/64/64/1',
-  'birthday': '900000',
-  'gender': '남자',
-  'job': '취준생생생'
-  },
-  {
-  'id': '2',
-  'name': '강주선',
-  'image': 'https://placeimg.com/64/64/2',
-  'birthday': '940305',
-  'gender': '남자',
-  'job': '취준생생'
-  },
-  {
-  'id': '3',
-  'name': '박철수',
-  'image': 'https://placeimg.com/64/64/3',
-  'birthday': '911210',
-  'gender': '남자',
-  'job': '취준생생생'
-  }
-]
 
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto"
   },
   table: {
@@ -60,6 +21,23 @@ const styles = theme => ({
 })
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -76,37 +54,14 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-              { customers.map(c => { return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); }) }
+              { this.state.customers ? this.state.customers.map(c => {
+                return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );
+                }) : ""}
           </TableBody>
         </Table>
       </Paper>
     );
   }
 }
-
-// function App() {
-//   // const { classes } = this.props;
-//   return (
-//     // <Paper className = { this.props.root }>
-//     //   <Table className = { this.table }>
-//     <Paper>
-//       <Table>
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>번호</TableCell>
-//             <TableCell>이미지</TableCell>
-//             <TableCell>이름</TableCell>
-//             <TableCell>생년월일</TableCell>
-//             <TableCell>성별</TableCell>
-//             <TableCell>직업</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//             { customers.map(c => { return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); }) }
-//         </TableBody>
-//       </Table>
-//     </Paper>
-//   );
-// }
 
 export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+        "id": 1,
+        "name": "홍길동",
+        "image": "https://placeimg.com/64/64/1",
+        "birthday": "900000",
+        "gender": "남자",
+        "job": "취준생생생"
+        },
+        {
+        "id": 2,
+        "name": "강주선",
+        "image": "https://placeimg.com/64/64/2",
+        "birthday": "940305",
+        "gender": "남자",
+        "job": "취준생생"
+        },
+        {
+        "id": 3,
+        "name": "박철수",
+        "image": "https://placeimg.com/64/64/3",
+        "birthday": "911210",
+        "gender": "남자",
+        "job": "취준생생생"
+        }
+    ])
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
