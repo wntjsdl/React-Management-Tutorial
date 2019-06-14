c4a974c7b1efa8c61c7c0f653638663aed3a369f
Jusun Kang <wntjs0305@gmail.com>
2019-06-14 17:41:22 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-        "id": 1,
-        "name": "홍길동",
-        "image": "https://placeimg.com/64/64/1",
-        "birthday": "900000",
-        "gender": "남자",
-        "job": "취준생생생"
-        },
-        {
-        "id": 2,
-        "name": "강주선",
-        "image": "https://placeimg.com/64/64/2",
-        "birthday": "940305",
-        "gender": "남자",
-        "job": "취준생생"
-        },
-        {
-        "id": 3,
-        "name": "박철수",
-        "image": "https://placeimg.com/64/64/3",
-        "birthday": "911210",
-        "gender": "남자",
-        "job": "취준생생생"
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ])
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
